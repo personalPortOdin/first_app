712177e7ca193daf20dd11b520a330f196b21079
portOdin <blur606@example.com>
2014-04-14 12:13:55 -0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Firstapp::Application.config.secret_key_base = 'c40f3ecfd97f5f821c997c92d7cf6e279bd68293a03d1af2c6d761119f5e61557930eedb872e3ecc87a743c86d8b55024d85c820aa346a0f19304b388a5ceb67'
